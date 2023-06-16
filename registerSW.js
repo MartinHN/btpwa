0090ba74b0e93eaa4acb7dcdb4c4781591008740
tinmar <martin.hermant@gmail.com>
2023-06-16 10:46:28 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/%3Crepo%3E/sw.js', { scope: '/%3Crepo%3E/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/btpwa/sw.js', { scope: '/btpwa/' })})}
